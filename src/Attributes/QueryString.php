3b1eb045a114d0e3c2b3b06ab019425b595652be
Matteo <outlaw.plz@gmail.com>
2025-02-04 18:44:40 +0100

    wip

--- src/Attributes/QueryString.php
+++ src/Attributes/QueryString.php
@@ -5,7 +5,7 @@ namespace Goodcat\QueryString\Attributes;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_METHOD|Attribute::IS_REPEATABLE)]
-class QueryString
+final readonly class QueryString
 {
     /**
      * @param non-empty-string $name
